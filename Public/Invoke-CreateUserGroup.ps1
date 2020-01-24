f483b5ab591eb81f10bdda70947c8fe27182e0ab
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-24 12:28:07 +0000

    Create Invoke-CreateUserGroup.ps1

---
+++ Public/Invoke-CreateUserGroup.ps1
@@ -0,0 +1,115 @@
+function Invoke-CreateUserGroup {
+  <#
+    .SYNOPSIS
+    Create user group
+
+    .DESCRIPTION
+    Wrapper function to create a new user group using the RiskPro batch client
+
+    .PARAMETER JavaPath
+    The optional java path parameter corresponds to the path to the Java executable file. If not specified, please ensure that the path contains the Java home.
+
+    .PARAMETER RiskProBatchClient
+    The RiskPro batch client parameter corresponds to the path to the RiskPro batch client JAR file.
+
+    .PARAMETER ServerURI
+    The server URI parameter corresponds to the Uniform Resource Identifier (URI) of the RiskPro server.
+
+    .PARAMETER Credentials
+    The credentials parameter corresponds to the credentials of the RiskPro account to use for the operation.
+
+    .PARAMETER JavaOptions
+    The optional Java options parameter corresponds to the additional Java options to pass to the Java client.
+
+    .PARAMETER GroupName
+    The group name parameter corresponds to the name of the user group to create.
+
+    .PARAMETER Users
+    The users parameter corresponds to the list of users to add to the user group.
+
+    .NOTES
+    File name:      Invoke-CreateUserGroup.ps1
+    Author:         Florian CARRIER
+    Creation date:  23/01/2020
+    Last modified:  23/01/2020
+  #>
+  [CmdletBinding (
+    SupportsShouldProcess = $true
+  )]
+  Param(
+    [Parameter (
+      Position    = 1,
+      Mandatory   = $false,
+      HelpMessage = "Java path"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [String]
+    $JavaPath,
+    [Parameter (
+      Position    = 2,
+      Mandatory   = $true,
+      HelpMessage = "RiskPro batch client path"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [Alias ("Path", "RiskProPath")]
+    [String]
+    $RiskProBatchClient,
+    [Parameter (
+      Position    = 3,
+      Mandatory   = $true,
+      HelpMessage = "RiskPro server URI"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [String]
+    $ServerURI,
+    [Parameter (
+      Position    = 4,
+      Mandatory   = $true,
+      HelpMessage = "Credentials of the user"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [System.Management.Automation.PSCredential]
+    $Credentials,
+    [Parameter (
+      Position    = 5,
+      Mandatory   = $false,
+      HelpMessage = "Java options"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [String[]]
+    $JavaOptions,
+    [Parameter (
+      Position    = 6,
+      Mandatory   = $true,
+      HelpMessage = "Name of the user group"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [Alias ("Name")]
+    [String]
+    $GroupName,
+    [Parameter (
+      Position    = 7,
+      Mandatory   = $false,
+      HelpMessage = "List of users to add"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [String[]]
+    $Users
+  )
+  Begin {
+    # Get global preference variables
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    # Get administration Java class
+    $JavaClass = Get-JavaClass -Name "Administration"
+  }
+  Process {
+    # Define operation parameters
+    $OperationParameters = New-Object -TypeName "System.Collections.Specialized.OrderedDictionary"
+    $OperationParameters.Add("ad.groupName", $GroupName)
+    if ($PSBoundParameters.ContainsKey("Users")) { $OperationParameters.Add("ad.users", $Users) }
+    # Format Java parameters
+    $Parameters = ConvertTo-JavaProperty -Properties $OperationParameters
+    # Create model
+  	Invoke-RiskProBatchClient -JavaPath $JavaPath -RiskProPath $RiskProBatchClient -ServerURI $ServerURI -Credentials $Credentials -JavaOptions $JavaOptions -Operation "createUserGroup" -Parameters $Parameters -Class $JavaClass
+  }
+}
